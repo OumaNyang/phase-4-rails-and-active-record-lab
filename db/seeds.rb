b3d5f04231704286c28e4b6754c144eed78dacf9
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-27 14:37:26 +0300

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding started'
+   Student.new(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
+   Student.new(first_name: 'Paul', last_name: 'Mutua', grade: 82)
+   Student.new(first_name: 'Suzzy', last_name: 'Akoth', grade: 76)
+
+puts 'Done seeding'
